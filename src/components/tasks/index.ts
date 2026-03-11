d626bc3442325dc5d73fb898fb5a1e19432cf9c7
Z User <z@container>
2026-03-11 12:43:45 +0000

    fix: 修复子组件导出问题 - 更新index.ts导出所有类型和工具函数

--- src/components/tasks/index.ts
+++ src/components/tasks/index.ts
@@ -5,3 +5,6 @@ export { UnlockDialog } from './UnlockDialog'
 export { ProcessUnlockDialog } from './ProcessUnlockDialog'
 export { FeedbackHistoryDialog } from './FeedbackHistoryDialog'
 export { UnlockRequestList } from './UnlockRequestList'
+
+// 导出类型和工具函数
+export * from './types'
